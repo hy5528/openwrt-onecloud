4d9f1c3f77b7bd818178427948e4406f1b970d96
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-04-02 09:25:11 +0800

    Update op3.sh

--- op3.sh
+++ op3.sh
@@ -23,19 +23,86 @@
 # Description: OpenWrt DIY script part 2 (rm Update feeds)
 #
 
-#=================================================
-sudo rm -rf feeds/packages/net/sing-box
-git clone https://github.com/orgx2812/sing-box feeds/packages/net/sing-box
-#sudo rm -rf feeds/packages/net/microsocks
-#sudo rm -rf feeds/packages/net/trojan-go
-#sudo rm -rf feeds/packages/net/v2ray-core
-#sudo rm -rf feeds/packages/net/v2ray-geodata
-#git clone https://github.com/orgx2812/v2ray-geodata feeds/packages/net/v2ray-geodata
-#sudo rm -rf feeds/packages/net/xray-core
-#git clone https://github.com/orgx2812/xray-core feeds/packages/net/xray-core
-#sudo rm -rf feeds/packages/net/chinadns-ng
-#sudo rm -rf feeds/luci/applications/luci-app-homeproxy
-sudo rm -rf feeds/packages/lang/golang
-git clone https://github.com/orgx2812/golang feeds/packages/lang/golang
-#sudo rm -rf feeds/packages/net/v2raya
-#git clone https://github.com/orgx2812/v2raya feeds/packages/net/v2raya
+# 删除自带的 golang
+rm -rf feeds/packages/lang/golang
+# 拉取新的 golang
+git clone https://github.com/sbwml/packages_lang_golang.git -b 26.x feeds/packages/lang/golang
+
+# 删除 passwall 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf package/feeds/packages/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+# 拉取新的 passwall-packages
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall-packages
+#cd package/passwall-packages
+#git checkout bc40fceb0488dfb5a4adb711cc1830a8021ee555
+#cd -
+
+# 删除 passwall 过时的 luci
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/feeds/luci/luci-app-passwall
+# 拉取新的 passwall-luci
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git package/passwall-luci
+#cd package/passwall-luci
+#git checkout ebd3355bdf2fcaa9e0c43ec0704a8d9d8cf9f658
+#cd -
+
+# 拉取 easytier、luci-app-easytier
+git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
+
+# 拉取锐捷认证
+git clone https://github.com/sbwml/luci-app-mentohust.git package/mentohust
+
+# 拉取 msd_lite、luci-app-msd_lite
+git clone https://github.com/gtolog/openwrt-msd_lite.git package/msd_lite
+#git clone https://github.com/gw826943555/openwrt_msd_lite.git package/msd_lite
+
+# 拉取 OpenAppFilter、luci-app-oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# 拉取 luci-app-tailscale
+git clone https://github.com/asvow/luci-app-tailscale.git package/luci-app-tailscale
+
+# 筛选提取应用
+## 删除自带的 ddns-scripts
+rm -rf feeds/packages/net/ddns-scripts
+## 删除自带的 luci-base
+rm -rf feeds/luci/modules/luci-base
+## 删除自带的 luci-mod-network
+#rm -rf feeds/luci/modules/luci-mod-network
+## 删除自带的 luci-app-firewall
+rm -rf feeds/luci/applications/luci-app-firewall
+## 删除自带的 luci-app-socat
+rm -rf feeds/lienol1/luci-app-socat
+## 筛选程序
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+## 提取 ddns-scripts
+merge_package openwrt-24.10 https://github.com/immortalwrt/packages.git feeds/packages/net net/ddns-scripts
+## 提取 fullconenat-nft
+merge_package openwrt-24.10 https://github.com/immortalwrt/immortalwrt.git package/network/utils package/network/utils/fullconenat-nft
+## 提取 pdnsd-alt、upx
+merge_package main https://github.com/kenzok8/jell.git package/kenzok8-package pdnsd-alt upx
+## 提取 luci-base
+merge_package openwrt-24.10 https://github.com/immortalwrt/luci.git feeds/luci/modules modules/luci-base
+## 提取 luci-mod-network
+#merge_package openwrt-24.10 https://github.com/immortalwrt/luci.git feeds/luci/modules modules/luci-mod-network
+## 提取 luci-app-firewall
+merge_package openwrt-24.10 https://github.com/immortalwrt/luci.git feeds/luci/applications applications/luci-app-firewall
+## 提取 luci-app-socat
+merge_package main https://github.com/chenmozhijin/luci-app-socat.git feeds/lienol1 luci-app-socat
